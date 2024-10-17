5af7ae26e14ff51d3d1d28a7547921ebd69c619b
Rony Romero Duque @ronygrafo <ronygrafo@icloud.com>
2024-10-17 15:26:23 -0500

    First commit: folder structure in project. UsingVITE

---
+++ PurpleFire/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      Hey, this is for my first commit
+    </>
+  )
+}
+
+export default App
--- README.md
+++
@@ -1,2 +0,0 @@
-# challenge-purple-fire
-Frontend Test: React-based UI with 2 screens, using Vanilla CSS and hexagonal architecture.
