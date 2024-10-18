cdcf04dbdafaf4df4769ad356a5fb0f9736767c2
Rony Romero Duque @ronygrafo <ronygrafo@icloud.com>
2024-10-18 17:42:04 -0500

    Implement basic navigation with React Router

--- PurpleFire/src/App.jsx
+++ PurpleFire/src/App.jsx
@@ -1,30 +1,53 @@
 /* eslint-disable no-unused-vars */
 
-import "./App.css";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link, useNavigate } from 'react-router-dom';
+import ProductPageNeedus from './ui/pages/ProductPageNeedus/ProductPageNeedus';
+import LandingPageAntic from './ui/pages/LandingPageAntic/LandingPageAntic';
+
 import PrimaryButton from "./ui/components/needus/PrimaryButton/PrimaryButton";
+
 import { MaterialSymbol } from "react-material-symbols";
 import "react-material-symbols/rounded";
 
-const handleClick = () => {
-  console.log("Clicked");
-};
-function App() {
+import "./App.css";
+
+function Navigation() {
+  const navigate = useNavigate();
+
+  const handleNeedusClick = () => {
+    navigate('/needus');
+  };
+
+  const handleAnticClick = () => {
+    navigate('/antic');
+  };
 
   return (
-    <>
+    <div>
       <PrimaryButton
         text="Needus PDP"
-        onClick={handleClick}
+        onClick={handleNeedusClick}
         className="primary-button"
       />
       <PrimaryButton
         text="Antic Landing Page"
-        onClick={handleClick}
+        onClick={handleAnticClick}
         className="secondary-button"
       />
-      <hr/>
+    </div>
+  );
+}
 
-      </>
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigation />} />
+        <Route path="/needus" element={<ProductPageNeedus />} />
+        <Route path="/antic" element={<LandingPageAntic />} />
+      </Routes>
+    </Router>
   );
 }
 
